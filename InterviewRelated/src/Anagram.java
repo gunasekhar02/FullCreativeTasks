417250f91bed50ddf55a1f1f5b6afdb2424cf052
Guna Sekhar Reddy <gunasekharreddy1122@gmail.com>
2023-04-10 23:31:45 +0530

    commit

--- InterviewRelated/src/Anagram.java
+++ InterviewRelated/src/Anagram.java
@@ -11,6 +11,7 @@ public class Anagram {
 		char[] str2=s2.toCharArray();
 		Arrays.sort(str1);
 		Arrays.sort(str2);
+		System.out.println (Arrays.equals(str1, str2));
 		for(int i=0;i<s1.length();i++) {
 			if(str1[i]!=str2[i]) {
 				return false;
@@ -25,6 +26,7 @@ public class Anagram {
 		Scanner sc=new Scanner(System.in);
 		String s1=sc.next();
 		String s2=sc.next();
+		
 		System.out.println (angramCheck(s1,s2));
 		
 		
