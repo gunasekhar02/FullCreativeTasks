66803838d22b00bbd475b9f530c1a5dd29e0b536
Guna Sekhar Reddy <gunasekharreddy1122@gmail.com>
2023-04-10 22:09:06 +0530

    commit

---
+++ InterviewRelated/src/EnumEx.java
@@ -0,0 +1,15 @@
+
+public class EnumEx {
+	public enum Season{summer , winter,spring,monsoon};
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(Season s:Season.values()) {
+			System.out.print(s+" ");
+		}
+		
+		
+		
+
+	}
+
+}
